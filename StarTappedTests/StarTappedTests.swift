5bbb0e6f056484b76db9fa2b93c98e77e2883a48
NovaFox161 <novamaday@gmail.com>
2019-01-05 15:37:03 -0600

    Initial Commit

---
+++ StarTappedTests/StarTappedTests.swift
@@ -0,0 +1,34 @@
+//
+//  StarTappedTests.swift
+//  StarTappedTests
+//
+//  Created by Nova Maday on 2019-01-05.
+//  Copyright © 2019 DreamExposure Studios. All rights reserved.
+//
+
+import XCTest
+@testable import StarTapped
+
+class StarTappedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
